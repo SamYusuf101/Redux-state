913befe091f1b824f77c017f65a4ba441f27930e
samyusuf101 <samyusuf101@gmail.com>
2023-03-14 04:08:49 -0700

    imported redux package

--- redux-state/src/index.js
+++ redux-state/src/index.js
@@ -2,30 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { createStore } from "redux";
 
-const increment = () => {
-  return {
-    type: "INCREMENT",
-  };
-};
-
-const decrement = () => {
-  return {
-    type: "DECREMENT",
-  };
-};
-
-const counter = (state, action) => {
-  switch (action.type) {
-    case "INCREMENT":
-      return state + 1;
-
-    case "DECREMENT":
-      return state - 1;
-    default:
-      return state;
-  }
-};
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
