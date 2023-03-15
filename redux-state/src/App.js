1de99d6213d341e103e2da445d8808487762e4ff
samyusuf101 <samyusuf101@gmail.com>
2023-03-15 01:36:32 -0700

    crested user action

--- redux-state/src/App.js
+++ redux-state/src/App.js
@@ -1,9 +1,32 @@
 import "./App.css";
+import { useSelector, useDispatch } from "react-redux";
+import { increment } from "./actions";
+import { decrement } from "./actions";
+import { setUser } from "./actions";
 
 function App() {
+  const counter = useSelector((state) => state.counter);
+  const user = useSelector((state) => state.user);
+
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
       <h1>React redux</h1>
+      <h1>Counter</h1>
+      <h2>{counter}</h2>
+      <div>
+        <button onClick={() => dispatch(increment())}>+</button>
+        <button onClick={() => dispatch(decrement())}>-</button>
+      </div>
+
+      {!user && (
+        <button onClick={() => dispatch(setUser({ id: 1, name: "Sam" }))}>
+          Set User
+        </button>
+      )}
+
+      {user && <p>{user.name}</p>}
     </div>
   );
 }
--- redux-state/src/reducers/users.js
+++ redux-state/src/reducers/users.js
@@ -2,6 +2,8 @@ const userReducer = (state = null, action) => {
   switch (action.type) {
     case "GET_USER":
       return state;
+    case "SET_USER":
+      return action.user;
     default:
       return state;
   }
